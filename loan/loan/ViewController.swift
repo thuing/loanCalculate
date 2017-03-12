a9fa496e491ff1ae5298642979a350b4c029a3aa
小福 <ht@xiaofudemacbook-pro.local>
2017-03-12 20:32:40 +0800

    第二版

--- loan/loan/ViewController.swift
+++ loan/loan/ViewController.swift
@@ -6,20 +6,42 @@
 //  Copyright © 2017年 小福. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loanAmountField: UITextField!
+    @IBOutlet weak var loanTermField: UITextField!
+    @IBOutlet weak var interestRateField: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func calculate(_ sender: Any) {
+        let calculate_1 = simpleCalculate()
+        result.text = calculate_1.calculate(loanAmount: Double( loanAmountField.text!)!, years: Int( loanTermField.text!)!, interestRate: Double( interestRateField.text!)!).description
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
+
+class simpleCalculate{
+    //小驼峰命名法：第一个英文单词小写，后面所有单词首字母大写
+    func  calculate(loanAmount:Double,years:Int,interestRate:Double) -> Double {
+        let rate = interestRate / 100
+        let interest = loanAmount * rate * Double(years)
+        return loanAmount + interest
+        
+    }
+}
