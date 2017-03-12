b0c81c9d06b157c4b5dbff127117d80defa2c76f
小福 <ht@xiaofudemacbook-pro.local>
2017-03-12 19:27:49 +0800

    1.0

---
+++ loan/loan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loan
+//
+//  Created by 小福 on 2017/3/12.
+//  Copyright © 2017年 小福. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
